907557dff94fb23f37e228bdfa3904beb63bb3f6
Ovoland <oceane.voland@epfl.ch>
2025-03-23 05:35:58 +0100

    Finished stage 1

---
+++ stages/stage1/enemies/electron/electron.gd
@@ -0,0 +1,39 @@
+extends RigidBody2D
+
+
+@export var center = Vector2.ZERO
+@export var radius = 0 
+@export var angle = 0.0
+@export var speed = 0
+
+@export var speedfactor = 0.6
+
+var health = 3
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	circular_movement()
+
+func electron():
+	pass
+
+func init(orbitCenter, orbitRadius, orbitAngle, orbitSpeed): 
+	center = orbitCenter
+	radius = orbitRadius
+	angle = orbitAngle
+	speed = orbitSpeed
+	
+	
+func circular_movement():
+	angle += speedfactor*speed * get_process_delta_time()
+	var x_pos = cos(angle)
+	var y_pos = sin(angle) #+ 0.1*sin(5*angle + PI/2) #for even more oscillation
+	
+	position.x = center.x + radius * x_pos
+	position.y = center.y + radius * y_pos
+		
