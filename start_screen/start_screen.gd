e3d1cc2c81cb1f1fc531b203cf01854d8a71279e
Ovoland <oceane.voland@epfl.ch>
2025-03-23 14:36:08 +0100

    Try to solve the merge conflict

--- start_screen/start_screen.gd
+++ start_screen/start_screen.gd
@@ -9,4 +9,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed('start_game'):
-		get_tree().change_scene_to_file('res://cutscenes/intro.tscn')
+		get_tree().change_scene_to_file('res://player/player.tscn')
